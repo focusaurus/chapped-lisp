26cc5f046a72bb835e69a46d7d7b67919a0df15d
Peter Lyons <pete@peterlyons.com>
2017-07-09 12:15:54 -0600

    Add CLI unit test

---
+++ code/cli-tap.js
@@ -0,0 +1,38 @@
+"use strict";
+const childProcess = require("child_process");
+const getStream = require("get-stream");
+const tap = require("tap");
+
+const cliPath = __filename.replace("-tap", "");
+
+tap.test("CLI integration test base case", test => {
+  const child = childProcess.spawn("node", [cliPath, "(+ 1 2)"]);
+  let exited = false;
+  let ended = false;
+  function done() {
+    if (exited && ended) {
+      test.end();
+    }
+  }
+  child.on("exit", code => {
+    tap.match(code, 0, "should exit zero success");
+    exited = true;
+    done();
+  });
+  getStream(child.stdout)
+    .then(out => {
+      ended = true;
+      tap.match(out, `[
+  "+",
+  1,
+  2
+]
+`);
+      done();
+    })
+    .catch(error => {
+      ended = true;
+      tap.error(error);
+      done();
+    });
+});
