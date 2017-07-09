821a5379fe3fd2e133e3cd063f419f60784feb5a
Peter Lyons <pete@peterlyons.com>
2017-07-09 12:31:06 -0600

    Use strictSame for tests

--- code/lex-tap.js
+++ code/lex-tap.js
@@ -3,19 +3,26 @@ const lex = require("./lex");
 const tap = require("tap");
 
 tap.test("lex basic cases", test => {
-  tap.same(lex("(+ 0 1)"), ["(", "+", "0", "1", ")"]);
-  tap.same(lex("(frog 9 8 7)"), ["(", "frog", "9", "8", "7", ")"]);
+  tap.strictSame(lex("(+ 0 1)"), ["(", "+", "0", "1", ")"]);
+  tap.strictSame(lex("(frog 9 8 7)"), ["(", "frog", "9", "8", "7", ")"]);
   test.end();
 });
 
 tap.test("lex basic cases whitespace loose", test => {
-  tap.same(lex("   (+\t0\n1 )  \n"), ["(", "+", "0", "1", ")"]);
-  tap.same(lex("\n\r\n( frog  9  8  7  ) "), ["(", "frog", "9", "8", "7", ")"]);
+  tap.strictSame(lex("   (+\t0\n1 )  \n"), ["(", "+", "0", "1", ")"]);
+  tap.strictSame(lex("\n\r\n( frog  9  8  7  ) "), [
+    "(",
+    "frog",
+    "9",
+    "8",
+    "7",
+    ")"
+  ]);
   test.end();
 });
 
 tap.test("lex nested case", test => {
-  tap.same(lex("(one 11 (two 21 22 23 )12 13)"), [
+  tap.strictSame(lex("(one 11 (two 21 22 23 )12 13)"), [
     "(",
     "one",
     "11",
--- code/parse-tap.js
+++ code/parse-tap.js
@@ -3,39 +3,45 @@ const parse = require("./parse");
 const tap = require("tap");
 
 tap.test("parseTokens basic flat cases", test => {
-  tap.same(parse(["(", "+", "0", "1", ")"]), ["+", "0", "1"]);
-  tap.same(parse(["(", "*", "42", "42", "666", ")"]), ["*", 42, 42, 666]);
-  tap.same(parse(["(", "max", "42", "42", "666", ")"]), ["max", 42, 42, 666]);
+  tap.strictSame(parse(["(", "+", "0", "1", ")"]), ["+", 0, 1]);
+  tap.strictSame(parse(["(", "*", "42", "42", "666", ")"]), ["*", 42, 42, 666]);
+  tap.strictSame(parse(["(", "max", "42", "42", "666", ")"]), [
+    "max",
+    42,
+    42,
+    666
+  ]);
   test.end();
 });
 
 tap.test("parseTokens nested cases", test => {
-  tap.same(parse(["(", "+", "0", "(", "-", "17", "18", ")", ")"]), [
+  tap.strictSame(parse(["(", "+", "0", "(", "-", "17", "18", ")", ")"]), [
     "+",
-    "0",
-    ["-", "17", "18"]
+    0,
+    ["-", 17, 18]
   ]);
-  tap.same(parse(["(", "*", "42", "42", "666", ")"]), ["*", 42, 42, 666]);
-  tap.same(parse(["(", "max", "42", "42", "666", ")"]), ["max", 42, 42, 666]);
+  tap.strictSame(parse(["(", "*", "42", "42", "666", ")"]), ["*", 42, 42, 666]);
+  tap.strictSame(parse(["(", "max", "42", "42", "666", ")"]), [
+    "max",
+    42,
+    42,
+    666
+  ]);
   test.end();
 });
 
 tap.test("parseTokens mixed values and sub-expressions", test => {
-  tap.same(parse(["(", "+", "11", "(", "-", "21", "22", ")", "12", ")"]), [
-    "+",
-    "11",
-    ["-", "21", "22"],
-    "12"
-  ]);
+  tap.strictSame(
+    parse(["(", "+", "11", "(", "-", "21", "22", ")", "12", ")"]),
+    ["+", 11, ["-", 21, 22], 12]
+  );
   test.end();
 });
 
 tap.test("parseTokens a deeper tree", test => {
-  tap.same(parse(["(", "+", "11", "(", "-", "21", "22", ")", "12", ")"]), [
-    "+",
-    11,
-    ["-", 21, 22],
-    12
-  ]);
+  tap.strictSame(
+    parse(["(", "+", "11", "(", "-", "21", "22", ")", "12", ")"]),
+    ["+", 11, ["-", 21, 22], 12]
+  );
   test.end();
 });
