9120029e50465065c19000e5f8d152d1be681160
Peter Lyons <pete@peterlyons.com>
2017-07-08 20:13:00 -0600

    Add code and tests for basic lexer and parser to AST
    Project setup: eslint static analysis, tap unit tests and coverage

--- chapped-lisp-tap.js
+++ chapped-lisp-tap.js
@@ -8,19 +8,81 @@ tap.test("lex basic cases", test => {
   test.end();
 });
 
-tap.test("parse basic flat cases", test => {
-  tap.same(lisp.parse(["(", "+", "0", "1", ")"]), ["+", "0", "1"]);
-  tap.same(lisp.parse(["(", "*", "42", "42", "666", ")"]), [
+tap.test("parseTokens basic flat cases", test => {
+  tap.same(lisp.parseTokens(["(", "+", "0", "1", ")"]), ["+", "0", "1"]);
+  tap.same(lisp.parseTokens(["(", "*", "42", "42", "666", ")"]), [
     "*",
     "42",
     "42",
     "666"
   ]);
-  tap.same(lisp.parse(["(", "max", "42", "42", "666", ")"]), [
+  tap.same(lisp.parseTokens(["(", "max", "42", "42", "666", ")"]), [
     "max",
     "42",
     "42",
     "666"
   ]);
   test.end();
+});
+
+tap.test("parseTokens nested cases", test => {
+  tap.same(lisp.parseTokens(["(", "+", "0", "(", "-", "17", "18", ")", ")"]), [
+    "+",
+    "0",
+    ["-", "17", "18"]
+  ]);
+  tap.same(lisp.parseTokens(["(", "*", "42", "42", "666", ")"]), [
+    "*",
+    "42",
+    "42",
+    "666"
+  ]);
+  tap.same(lisp.parseTokens(["(", "max", "42", "42", "666", ")"]), [
+    "max",
+    "42",
+    "42",
+    "666"
+  ]);
+  test.end();
+});
+
+tap.test("parseTokens mixed values and sub-expressions", test => {
+  tap.same(
+    lisp.parseTokens(["(", "+", "11", "(", "-", "21", "22", ")", "12", ")"]),
+    ["+", "11", ["-", "21", "22"], "12"]
+  );
+  test.end();
+});
+
+tap.test("parseTokens a deeper tree", test => {
+  tap.same(
+    lisp.parseTokens(["(", "+", "11", "(", "-", "21", "22", ")", "12", ")"]),
+    ["+", "11", ["-", "21", "22"], "12"]
+  );
+  test.end();
+});
+
+tap.test("full parse basic expression", {skip: false}, test => {
+  tap.same(lisp.parse("(one 11 12)"), ["one", "11", "12"]);
+  test.end();
+});
+
+tap.test("full parse medium complex nesting", {skip: false}, test => {
+  tap.same(lisp.parse("(one 11 12 (two 22 (three 33 34 35) 23) 13)"), [
+    "one",
+    "11",
+    "12",
+    ["two", "22", ["three", "33", "34", "35"], "23"],
+    "13"
+  ]);
+  test.end();
+});
+
+tap.test("syntax error: missing closing paren", {skip: false}, test => {
+  try {
+    lisp.parse("(add 1 2");
+    tap.fail("Expected syntax error");
+  } catch (error) {
+    test.end();
+  }
 });
--- chapped-lisp.js
+++ chapped-lisp.js
@@ -1,8 +1,8 @@
-// (+ 3 4)
-// (xxx start function
-//  3 4 args
-// ) end function call
+#!/usr/bin/env node
+"use strict";
+const debug = require("debug")("chapped-lisp");
 
+// Helper function to lex a start function call
 function startFunction(token) {
   if (token.startsWith("(")) {
     return ["(", token.slice(1)];
@@ -10,23 +10,42 @@ function startFunction(token) {
   return [token];
 }
 
+// Helper function to lex a series of end function calls
 function endFunction(tokens) {
   let nodes = [];
   tokens.forEach(token => {
-    nodes = nodes.concat(token.split(")").map(x => (x ? x : ")")));
+    nodes = nodes.concat(token.split(")").map(x => x || ")"));
   });
 
   return nodes;
 }
 
+/**
+ * Convert a string of chapped lisp source code to an array of tokens.
+ *
+ * Lex takes a string of chapped lisp source code
+ * and converts it into an array of tokens
+ * It mostly deals with whitespace and parens vs other values
+ *
+ * Given "(+ 3 4)"
+ * We get ["(", "+", "3", "4", ")"] as the output token list
+ * Given (tag 11 (bag 21 22) 12)
+ * we get [ "(", "tag", "11", "(", "bag", "21", "22", ")", "12", ")" ]
+*/
 function lex(lisp) {
-  const tree = [];
-  lisp.split(/\s/g).map(startFunction).map(endFunction).forEach(subArray => {
-    subArray.forEach(value => {
-      tree.push(value);
+  const tokens = [];
+  lisp
+    .split(/\s/g) // split on whitespace
+    .map(startFunction) // break "(name" tokens into ["(", "name"]
+    .map(endFunction) // break "4)))" into ["4", ")", ")", ")"]
+    .forEach(subArray => {
+      subArray.forEach(value => {
+        // flatten into a single file array
+        tokens.push(value);
+      });
     });
-  });
-  return tree;
+  debug(`lisp:\n${lisp}\n----\ntokens:\n${tokens}`);
+  return tokens;
 }
 
 exports.lex = lex;
@@ -35,28 +54,45 @@ function syntaxError(message) {
   return new Error(`Invalid syntax: ${message}`);
 }
 
-function parse(tokens) {
+// Parse is a recursive function to take a flat list of tokens and turn it
+// into a nested syntax tree. The recursion is how the flat becomes nested.
+function parseTokens(tokens, depth = 0) {
   // input: ["(", "+", "0", "1", ")"]
   // output: ["+", "0", "1"]
-  let nodes = [];
-  for (var i = 1; i < tokens.length; i++) {
-    const token = tokens[i];
-    switch (token) {
+  // input: ["(", "+", "0", "(", "-", "4", "3")")"]
+  // output: ["+", "0", ["-", "4", "3"]]
+  const nodes = [];
+  while (tokens.length) {
+    switch (tokens[0]) {
       case "(":
-        nodes.push(parse(tokens.slice(i)));
+        if (!nodes.length) {
+          // first open paren, start an array
+          tokens.shift(); // consume the open paren
+        } else {
+          // nested expression, leave the open paren there and recur
+          nodes.push(parseTokens(tokens, depth + 1));
+        }
+        break;
       case ")":
+        tokens.shift(); // consume the close paren, we're done
         return nodes;
       default:
-        nodes.push(token);
+        nodes.push(tokens.shift()); // consume the close paren, we're done
     }
   }
-  return nodes;
+  throw syntaxError("missing closing paren");
 }
+exports.parseTokens = parseTokens;
+
+function parse(chappedLispSource) {
+  return parseTokens(lex(chappedLispSource));
+}
+
+// main exported API
 exports.parse = parse;
 
+/* istanbul ignore if */
 if (require.main === module) {
-  const tokens = lex(process.argv[2]);
-  console.log(JSON.stringify(tokens, null, 2));
-  const tree = parse(tokens);
-  console.log("----\n", JSON.stringify(tree, null, 2));
+  const tree = parse(process.argv[2]);
+  console.log(JSON.stringify(tree, null, 2));
 }
