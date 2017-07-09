93e10d6ad29f67ba3479371d9d0d98b1cb864ebe
Peter Lyons <pete@peterlyons.com>
2017-07-09 12:21:19 -0600

    Extract parseScalar function

--- code/parse.js
+++ code/parse.js
@@ -4,6 +4,12 @@ function syntaxError(message) {
   return new Error(`Invalid syntax: ${message}`);
 }
 
+function parseScalar(scalarString) {
+  // Only supports numbers currently, but in future could add booleans, strings
+  const value = parseFloat(scalarString);
+  return isNaN(value) ? scalarString : value;
+}
+
 // Parse is a recursive function to take a flat list of tokens and turn it
 // into a nested syntax tree. The recursion is how the flat becomes nested.
 function parse(tokens, depth = 0) {
@@ -27,9 +33,8 @@ function parse(tokens, depth = 0) {
         tokens.shift(); // consume the close paren, we're done
         return nodes;
       default: {
-        const token = tokens.shift(); // consume the close paren, we're done
-        const value = parseFloat(token);
-        nodes.push(isNaN(value) ? token : value);
+        const token = tokens.shift();
+        nodes.push(parseScalar(token));
       }
     }
   }
